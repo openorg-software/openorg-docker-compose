79b2c11385a807978faab2524cecd43a3cb437d8
Benjamin Schilling <benjamin.schilling33@gmail.com>
2023-01-22 19:39:24 +0100

    update letsencrypt

--- letsencrypt-proxy/init.sh
+++ letsencrypt-proxy/init.sh
@@ -2,6 +2,9 @@
 
 # Create all directories
 mkdir -p acme.sh
-
+mkdir -p ./nginx/certs
+mkdir -p ./nginx/vhost.d
+mkdir -p ./nginx/html
+mkdir -p ./nginx/conf.d
 # Create network
 docker network create proxy
